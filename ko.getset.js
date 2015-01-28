b7279668b7f40a6813ef17b98f23619a84bd23f1
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2015-01-28 18:51:43 +0200

    initial commit

---
+++ ko.getset.js
@@ -0,0 +1,113 @@
+(function (factory) {
+    // Module systems magic dance.
+
+    if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
+        // CommonJS or Node: hard-coded dependency on "knockout"
+        factory(require("knockout"), exports);
+    } else if (typeof define === "function" && define["amd"]) {
+        // AMD anonymous module with hard-coded dependency on "knockout"
+        define(["knockout", "exports"], factory);
+    } else {
+        // <script> tag: use the global `ko` object
+        factory(ko, ko);
+    }
+}(function (ko, exports) {
+    function gsObservable(object, key, initValue){
+        initValue = initValue || null;
+
+        if (typeof object !== 'object'){
+            throw new Error('Invalid argument: parent object must be set');
+        }
+
+        if (typeof key !== 'string' || key.length === 0){
+            throw new Error('Invalid argument: property key must be a string');
+        }
+
+        if (object[key]){
+            throw new Error('Invalid argument: key ' + key + ' already set');
+        }
+
+        var kovar = ko.observable(initValue);
+
+        Object.defineProperty(object, key, {
+            get: function(){
+                return kovar();
+            },
+            set: function(val){
+                kovar(val);
+            },
+            enumerable: true
+        });
+
+        return kovar;
+    }
+
+    function gsComputed(object, key, handler, context){
+        var kovar;
+
+        if (typeof object !== 'object'){
+            throw new Error('Invalid argument: parent object must be set');
+        }
+
+        if (typeof key !== 'string' || key.length === 0){
+            throw new Error('Invalid argument: property key must be a string');
+        }
+
+        if (object[key]){
+            throw new Error('Invalid argument: key ' + key + ' already set');
+        }
+
+        if (typeof handler !== 'function'){
+            throw new Error('Invalid argument: computed handler must be a function');
+        }
+
+        if (context){
+            kovar = ko.computed(handler, context);
+        } else {
+            kovar = ko.computed(handler, object);
+        }
+
+        Object.defineProperty(object, key, {
+            get: function(){
+                return kovar();
+            },
+            enumerable: true
+        });
+
+        return kovar;
+    }
+
+    function gsObservableArray(object, key, initValue){
+        initValue = initValue || [];
+
+        if (typeof object !== 'object'){
+            throw new Error('Invalid argument: parent object must be set');
+        }
+
+        if (typeof key !== 'string' || key.length === 0){
+            throw new Error('Invalid argument: property key must be a string');
+        }
+
+        if (object[key]){
+            throw new Error('Invalid argument: key ' + key + ' already set');
+        }
+
+        var kovar = ko.observableArray(initValue);
+
+        Object.defineProperty(object, key, {
+            get: function(){
+                return kovar();
+            },
+            set: function(val){
+                kovar(val);
+            },
+            enumerable: true
+        });
+
+        return kovar;
+    }
+
+    exports.gsObservable = gsObservable;
+    exports.gsComputed = gsComputed;
+    exports.gsObservableArray = gsObservableArray;
+}));
---
+++ readme.md
@@ -0,0 +1,101 @@
+#knockout.getset
+
+Simple library to start using `this.property = true;` instead of `this.property(true);` for observables in [Knockout](https://github.com/knockout/knockout).
+
+##Installation
+
+Install from `bower`:
+
+```bash
+bower install knockout.getset --save
+```
+
+or `npm`:
+
+```bash
+npm install knockout.getset --save
+```
+
+##Compatibility
+Internet Explorer 9+ and literaly anything else. For more information [consult this table](http://kangax.github.io/compat-table/es5/#Getter_in_property_initializer).
+
+##Documentation
+
+###Observable
+To define an observable you need:
+```js
+var viewmodel = {},
+    initialValue = 1;
+ko.gsObservable(viewmodel, 'observableProperty', initialValue);
+```
+
+Then `observableProperty` will be available to modification in `viewModel` object:
+
+```js
+    viewmodel.observableProperty = 1;
+    viewmodel.observableProperty; //1
+    
+    viewmodel.observableProperty = 5;
+    viewmodel.observableProperty; //5
+```
+
+If you want to register your own subscriptions with `gsObservable`:
+
+```js
+var viewModel = {},
+    initialValue = 1;
+ko.gsObservable(viewModel, 'observableProperty', initialValue)
+    .subscribe(function(newValue){
+        console.log('I\'ve been changed to %s', newValue);
+    });
+    
+    viewmodel.observableProperty = 5;
+    //I've been changed to 5
+```
+
+###Observable Array
+To define an observable array you need:
+```js
+var viewmodel = {};
+ko.gsObservableArray(viewmodel, 'observableArray');
+
+viewmodel.observableArray; //[]
+```
+
+###Computed
+To define a computed you need:
+```js
+var viewmodel = {};
+ko.gsComputed(viewmodel, 'computed', function(){
+    return 'I\'m computed!';
+});
+
+viewmodel.computed; //"I'm computed!"
+```
+
+Example on using of computed with observables:
+```js
+var viewmodel = {};
+ko.gsObservable(viewmodel, 'name', 'Peter Griffin');
+ko.gsComputed(viewmodel, 'title', function(){
+    return 'Mr. ' + this.name;
+});
+
+viewmodel.title; "Mr. Peter Griffin"
+
+viewmodel.name = 'Brian Griffin';
+viewmodel.title; "Mr. Brian Griffin"
+```
+
+###Simple objects
+Since there is no observables in viewmodel then we can simply skip a 'ko.toJS' or any other unwrap routine. In example:
+```js
+var viewmodel = {};
+ko.gsObservable(viewmodel, 'name', 'Peter Griffin');
+ko.gsComputed(viewmodel, 'title', function(){
+    return 'Mr. ' + this.name;
+});
+
+JSON.stringify(viewmodel);
+//'{ "name":"Peter Griffin", "title":"Mr. Peter Griffin" }'
+```
